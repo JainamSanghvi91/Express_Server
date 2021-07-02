42cbfabaef517e7624db7b19dd27a92ac46ae411
Jainam Sanghvi <jainam1224@gmail.com>
2021-07-02 20:58:30 +0530

    Bug Solved

--- index.js
+++ index.js
@@ -14,48 +14,6 @@ const dishRouter = require('./routes/dishRouter');
 
 app.use('/dishes', dishRouter);
 
-app.all('/dishes', (req,res,next) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  next();
-});
-
-app.get('/dishes', (req,res,next) => {
-    res.end('Will send all the dishes to you!');
-});
-
-app.post('/dishes', (req, res, next) => {
- res.end('Will add the dish: ' + req.body.name + ' with details: ' + req.body.description);
-});
-
-app.put('/dishes', (req, res, next) => {
-  res.statusCode = 403;
-  res.end('PUT operation not supported on /dishes');
-});
- 
-app.delete('/dishes', (req, res, next) => {
-    res.end('Deleting all dishes');
-});
-
-app.get('/dishes/:dishId', (req,res,next) => {
-    res.end('Will send details of the dish: ' + req.params.dishId +' to you!');
-});
-
-app.post('/dishes/:dishId', (req, res, next) => {
-  res.statusCode = 403;
-  res.end('POST operation not supported on /dishes/'+ req.params.dishId);
-});
-
-app.put('/dishes/:dishId', (req, res, next) => {
-  res.write('Updating the dish: ' + req.params.dishId + '\n');
-  res.end('Will update the dish: ' + req.body.name + 
-        ' with details: ' + req.body.description);
-});
-
-app.delete('/dishes/:dishId', (req, res, next) => {
-    res.end('Deleting dish: ' + req.params.dishId);
-});
-
 //Here we can see we have done 2 setups for app.use() and it will work in order top to bottom like if we found the URL that satisfies
 //in 1st setup then the result will be given by that otherwise it will go in 2nd setup and so on .. because we have not handled the case
 //for incorrect url .. etc issues (errors)
